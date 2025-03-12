82d14dbb773b0a9056b25f32b014e0ae0b01d970
raiworkgit <raizelfaithd@gmail.com>
2025-03-12 15:44:19 +0800

    Add files via upload

--- src/js/data.js
+++ src/js/data.js
@@ -13,100 +13,201 @@ export const user = [
 ];
 
 export const products = [
+  // ADIDAS MEN
   {
     id: 0,
-    itemName: "Nike Jordan V1",
-    sizes: ["small", "medium", "large"],
-    price: 100,
+    itemName: "Samba OG Shoes",
+    sizes: ["5UK", "6UK", "6.5UK"],
+    price: "₱6,800",
     quantity: 100,
-    brand: "Nike",
+    brand: "Adidas",
+    ratingCount: 250,
+    description: "Born on the pitch, the Samba is a timeless icon of street style. This silhouette stays true to its legacy with a tasteful, low-profile, soft leather upper, suede overlays, and gum sole, making it a staple in everyone's closet - on and off the pitch.",
+    imgSrc: [
+        "src/assets/images/ADIDAS-MEN-1.avif",  // Front view
+        "src/assets/images/ADIDAS-MEN-1.2.avif",   // Side view
+        "src/assets/images/ADIDAS-MEN-1.3.avif",   // Back view
+        "src/assets/images/ADIDAS-MEN-1.4.avif"     // Top view
+    ]
   },
   {
     id: 1,
-    itemName: "Nike Air Max 270",
-    sizes: ["small", "medium", "large"],
-    price: 130,
-    quantity: 90,
-    brand: "Nike",
+    itemName: "Samba Adizero Adios Pro 4 Shoes",
+    sizes: ["5UK", "6UK", "6.5UK"],
+    price: "₱13,000",
+    quantity: 100,
+    brand: "Adidas",
+    ratingCount: 250,
+    description: "Proven to be the world's most winning shoe, the Adizero Adios Pro line is the pinnacle of Adizero Racing shoes. The Adios Pro 4 is made for fast runners who want to experience faster, with enhanced features designed to optimize running efficiency. ",
+    imgSrc: [
+        "src/assets/images/ADIDAS-MEN-2.avif",  // Front view
+        "src/assets/images/ADIDAS-MEN-2.2.avif",   // Side view
+        "src/assets/images/ADIDAS-MEN-2.3.avif",   // Back view
+        "src/assets/images/ADIDAS-MEN-2.4.avif"     // Top view
+    ]
   },
   {
     id: 2,
-    itemName: "Adidas Ultraboost 22",
-    sizes: ["medium", "large"],
-    price: 120,
-    quantity: 75,
+    itemName: "Superstar 82 Shoes",
+    sizes: ["5UK", "6UK", "6.5UK"],
+    price: "₱9,000",
+    quantity: 100,
     brand: "Adidas",
+    ratingCount: 250,
+    description: "The iconic silhouette gets done up with a real denim upper, and goes even further with stitched detailing throughout to mimic chain-stitching on truly premium denim silhouettes.",
+    imgSrc: [
+        "src/assets/images/ADIDAS-MEN-3.avif",  // Front view
+        "src/assets/images/ADIDAS-MEN-3.2.avif",   // Side view
+        "src/assets/images/ADIDAS-MEN-3.3.avif",   // Back view
+        "src/assets/images/ADIDAS-MEN-3.4.avif"     // Top view
+    ]
   },
   {
     id: 3,
-    itemName: "Adidas NMD_R1",
-    sizes: ["medium", "large"],
-    price: 110,
-    quantity: 140,
+    itemName: "Ultraboost 1.0 Fortnite Shoes",
+    sizes: ["5UK", "6UK", "6.5UK"],
+    price: "₱10,000",
+    quantity: 100,
     brand: "Adidas",
+    ratingCount: 250,
+    description: "Adidas x Fortnite shoes made for supreme, daily comfort. You're always in game mode with these ultra-comfortable shoes, part of a collaboration between adidas and Fortnite.",
+    imgSrc: [
+        "src/assets/images/ADIDAS-MEN-4.avif",  // Front view
+        "src/assets/images/ADIDAS-MEN-4.2.avif",   // Side view
+        "src/assets/images/ADIDAS-MEN-4.3.avif",   // Back view
+        "src/assets/images/ADIDAS-MEN-4.4.avif"     // Top view
+    ]
   },
   {
     id: 4,
-    itemName: "Puma RS-X3",
-    sizes: ["small", "medium"],
-    price: 90,
-    quantity: 150,
-    brand: "Puma",
+    itemName: "Harden Volume 9 Shoes",
+    sizes: ["5UK", "6UK", "6.5UK"],
+    price: "₱9,500",
+    quantity: 100,
+    brand: "Adidas",
+    ratingCount: 250,
+    description: "Continuing to build on his legend and legacy, the latest signature shoes from James Harden and adidas Basketball are designed for players who live for the game's biggest moments.",
+    imgSrc: [
+        "src/assets/images/ADIDAS-MEN-5.avif",  // Front view
+        "src/assets/images/ADIDAS-MEN-5.2.avif",   // Side view
+        "src/assets/images/ADIDAS-MEN-5.3.avif",   // Back view
+        "src/assets/images/ADIDAS-MEN-5.4.avif"     // Top view
+    ]
   },
+
+  //ADIDAS WOMEN
   {
     id: 5,
-    itemName: "Puma Future Rider",
-    sizes: ["small", "medium", "large"],
-    price: 100,
-    quantity: 120,
-    brand: "Puma",
+    itemName: "Samba OG Shoes",
+    sizes: ["5UK", "6UK", "6.5UK"],
+    price: "₱6,800",
+    quantity: 100,
+    brand: "Adidas",
+    ratingCount: 250,
+    description: "The adidas Samba OG Shoes are a casual icon reimagined for sunny, fair weather days. The breathable cotton mesh upper keeps you cool while suede overlays add structure and durability over the toe and along the laces.",
+    imgSrc: [
+        "src/assets/images/ADIDAS-WOMEN-1.avif",  // Front view
+        "src/assets/images/ADIDAS-WOMEN-1.2.avif",   // Side view
+        "src/assets/images/ADIDAS-WOMEN-1.3.avif",   // Back view
+        "src/assets/images/ADIDAS-WOMEN-1.4.avif"     // Top view
+    ]
   },
   {
     id: 6,
-    itemName: "Vans Old Skool",
-    sizes: ["small", "medium", "large"],
-    price: 65,
-    quantity: 180,
-    brand: "Vans",
+    itemName: "Adizero Adios Pro 4 Shoes",
+    sizes: ["5UK", "6UK", "6.5UK"],
+    price: "₱13,000",
+    quantity: 100,
+    brand: "Adidas",
+    ratingCount: 250,
+    description: "Proven to be the world's most winning shoe, the Adizero Adios Pro line is the pinnacle of Adizero Racing shoes. The Adios Pro 4 is made for fast runners who want to experience faster, with enhanced features designed to optimize running efficiency. ",
+    imgSrc: [
+        "src/assets/images/ADIDAS-WOMEN-2.avif",  // Front view
+        "src/assets/images/ADIDAS-WOMEN-2.2.avif",   // Side view
+        "src/assets/images/ADIDAS-WOMEN-2.3.avif",   // Back view
+        "src/assets/images/ADIDAS-WOMEN-2.4.avif"     // Top view
+    ]
   },
   {
     id: 7,
-    itemName: "Vans Authentic",
-    sizes: ["small", "medium"],
-    price: 55,
-    quantity: 250,
-    brand: "Vans",
+    itemName: "Handball Spezial Shoes",
+    sizes: ["5UK", "6UK", "6.5UK"],
+    price: "₱5,800",
+    quantity: 100,
+    brand: "Adidas",
+    ratingCount: 250,
+    description: "This low-profile trainer brings energy and comfort to your style game. Originally designed for fast-paced indoor sports, these adidas Handball Spezial Shoes playing days are done but keep your style game strong. Crafted for comfort, their leather and textile upper surrounds a synthetic lining.",
+    imgSrc: [
+        "src/assets/images/ADIDAS-WOMEN-3.avif",  // Front view
+        "src/assets/images/ADIDAS-WOMEN-3.2.avif",   // Side view
+        "src/assets/images/ADIDAS-WOMEN-3.3.avif",   // Back view
+        "src/assets/images/ADIDAS-WOMEN-3.4.avif"     // Top view
+    ]
   },
   {
     id: 8,
-    itemName: "Skechers Go Walk",
-    sizes: ["small", "medium", "large"],
-    price: 55,
-    quantity: 250,
-    brand: "Skechers",
+    itemName: "Taekwondo Shoes",
+    sizes: ["5UK", "6UK", "6.5UK"],
+    price: "₱6,500",
+    quantity: 100,
+    brand: "Adidas",
+    ratingCount: 250,
+    description: "Minimal, super low-profile trainers crafted from soft, supple leather. Slip into a slice of adidas history with these Taekwondo Shoes. The sleek leather upper and ultra-low profile outsole provide a minimalist look inspired by an early 2000s icon.",
+    imgSrc: [
+        "src/assets/images/ADIDAS-WOMEN-4.avif",  // Front view
+        "src/assets/images/ADIDAS-WOMEN-4.2.avif",   // Side view
+        "src/assets/images/ADIDAS-WOMEN-4.3.avif",   // Back view
+        "src/assets/images/ADIDAS-WOMEN-4.4.avif"     // Top view
+    ]
   },
   {
     id: 9,
-    itemName: "Fila Disruptor 2",
-    sizes: ["medium", "large"],
-    price: 75,
-    quantity: 95,
-    brand: "Fila",
+    itemName: "Subzone Shoes",
+    sizes: ["5UK", "6UK", "6.5UK"],
+    price: "₱3,400",
+    quantity: 100,
+    brand: "Adidas",
+    ratingCount: 250,
+    description: "Sleek and comfortable sneakers that support your dynamic movements. Inspired by performance basketball shoes, these adidas sneakers are designed to support your every move. The upper is made from a mix of materials that hug the foot in place while allowing air to circulate. ",
+    imgSrc: [
+        "src/assets/images/ADIDAS-WOMEN-5.avif",  // Front view
+        "src/assets/images/ADIDAS-WOMEN-5.1.avif",   // Side view
+        "src/assets/images/ADIDAS-WOMEN-5.3.avif",   // Back view
+        "src/assets/images/ADIDAS-WOMEN-5.4.avif"     // Top view
+    ]
+  },
+
+  //
+  {
+    id: 5,
+    itemName: "Samba OG Shoes",
+    sizes: ["5UK", "6UK", "6.5UK"],
+    price: "₱6,800",
+    quantity: 100,
+    brand: "Adidas",
+    ratingCount: 250,
+    description: "The adidas Samba OG Shoes are a casual icon reimagined for sunny, fair weather days. The breathable cotton mesh upper keeps you cool while suede overlays add structure and durability over the toe and along the laces.",
+    imgSrc: [
+        "src/assets/images/ADIDAS-MEN-5.avif",  // Front view
+        "src/assets/images/ADIDAS-MEN-5.2.avif",   // Side view
+        "src/assets/images/ADIDAS-MEN-5.3.avif",   // Back view
+        "src/assets/images/ADIDAS-MEN-5.4.avif"     // Top view
+    ]
   },
   {
-    id: 10,
-    itemName: "Saucony Endorphin Speed",
-    sizes: ["small", "medium"],
-    price: 140,
-    quantity: 110,
-    brand: "Saucony",
-  },
-  {
-    id: 11,
-    itemName: "Asics Gel-Kayano 27",
-    sizes: ["small", "medium"],
-    price: 160,
-    quantity: 60,
-    brand: "Asics",
+    id: 5,
+    itemName: "Samba OG Shoes",
+    sizes: ["5UK", "6UK", "6.5UK"],
+    price: "₱6,800",
+    quantity: 100,
+    brand: "Adidas",
+    ratingCount: 250,
+    description: "The adidas Samba OG Shoes are a casual icon reimagined for sunny, fair weather days. The breathable cotton mesh upper keeps you cool while suede overlays add structure and durability over the toe and along the laces.",
+    imgSrc: [
+        "src/assets/images/ADIDAS-MEN-5.avif",  // Front view
+        "src/assets/images/ADIDAS-MEN-5..avif",   // Side view
+        "src/assets/images/ADIDAS-MEN-5.3.avif",   // Back view
+        "src/assets/images/ADIDAS-MEN-5.4.avif"     // Top view
+    ]
   },
 ];
